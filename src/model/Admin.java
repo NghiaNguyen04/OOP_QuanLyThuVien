3f16c979a5841b1ab80409d26196c76b6bc466f4
vanmuoi24 <domuoigghh@gmail.com>
2023-10-12 14:03:24 +0700

    up

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -3,4 +3,17 @@ package model;
 public class Admin {
     private String adminID;
     private String password;
+    public String getAdminID() {
+        return adminID;
+    }
+    public void setAdminID(String adminID) {
+        this.adminID = adminID;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
