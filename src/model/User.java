04a6450bf6a5c72e907e27bdeb9c03370246df97
NghiaNguyen04 <tuannghia20042015@gmail.com>
2023-10-13 12:07:13 +0700

    update User

--- src/model/User.java
+++ src/model/User.java
@@ -8,4 +8,73 @@ public class User {
     private String phone;
     private String email;
     private String address;
+
+    public User() {
+    }
+
+    public User(String userID, String password, String surName, String firstName, String phone, String email, String address) {
+        this.userID = userID;
+        this.password = password;
+        this.surName = surName;
+        this.firstName = firstName;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
